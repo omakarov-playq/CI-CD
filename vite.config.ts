ac61821dab9d08759edeeddae78a2a23c9d86ac4
omakarov <omakarov@playq.com>
2026-03-22 19:03:39 +0200

    fix settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/omakarov-playq/CI-CD.git',
+  base: '/CI-CD/',
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
